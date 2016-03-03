c5d70f603f6affb1965ddff940f4f8bec88299db
Jure Markun <jure.markun@gmail.com>
2016-03-03 21:47:00 +0100

    commit datoteke gui

---
+++ gui.py
@@ -0,0 +1,27 @@
+###### Razred GUI: <h4>
+##Vmesnik bo vseboval metode, s katerimi bo risal poteze na platno. Celotni razred skupaj z metodami bo vsebovan v (glavni) datoteki dama.py. Uporabljene metode bodo:
+##* izbira_igralca(self)
+##* zacni_igro(self)
+##* premakni_figuro(self,i,j)
+##* koncaj_igro(self, zmagovalec)
+##
+
+import tkinter
+#import dama
+
+class Gui():
+##        def __init__(self, master):
+##            pass
+##
+##    # igralno območje
+##            self.deska = tkinter.Canvas(master, width=500, height=500)
+##            self.deska.grid(row=1,column=1)
+##            
+    pass
+
+   
+root = tkinter.Tk()
+root.title("Dama")
+aplikacija = Gui()
+root.mainloop()
+# more bit Gui(root), sam piše da nima argumentov
