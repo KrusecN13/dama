160e29daa921656b88cbf8b5fd2d985ebdfedb83
Jure Markun <jure.markun@gmail.com>
2016-03-06 00:18:58 +0100

    spremeni dimenzijo polja, začne z delom na gumbih in napisih (še ne dela), osnutek metod

--- gui.py
+++ gui.py
@@ -1,43 +1,52 @@
 ###### Razred GUI: <h4>
 ##Vmesnik bo vseboval metode, s katerimi bo risal poteze na platno.
 #Celotni razred skupaj z metodami bo vsebovan v (glavni) datoteki dama.py.
-#Uporabljene metode bodo:
-##* izbira_igralca(self)
-##* zacni_igro(self)
-##* premakni_figuro(self,i,j)
-##* koncaj_igro(self, zmagovalec)
-##
+
 
 from Tkinter import *
 #import dama
 #Definiramo razred, ki predstavlja našo aplikacijo
-class Plosca():
+class Gui():
         def __init__(self, master=None):
-           self.canvas = Canvas(master, width=1000, height=1000)
-           self.canvas.pack()
-           for x in range(0,1000,125):
-               for y in range(0,1000,125):
-                   if (x+y) % 2 == 0:
-                       self.canvas.create_rectangle(x+125,y+125,
+            #init = stvari ki se zgodijo enkrat, vse kar se more zgodit, da
+            #zaženemo grafièni vmesnik
+
+            self.canvas = Canvas(master, width=1000, height=800)
+            self.canvas.pack()
+            for x in range(0,800,100):
+                for y in range(0,800,100):
+                    if ((x+y)//100)%2 == 0:
+                       self.canvas.create_rectangle(x+(100),y+(100),
                                                     x,y,
                                                     outline="#000000", fill="#000000")
 
-                   else:
-                       self.canvas.create_rectangle(x+125,y+125,
+                    else:
+                       self.canvas.create_rectangle(x+(100),y+(100),
                                                     x,y,
                                                     outline="#ffffff", fill="#ffffff")
-##           self.beli = Button(master, text="beli") 
-##           for x in range(125/2,1000, 125):
-##                for y in range(125/2,250 + 125/2, 125):
-##                     if (x+y) % 2 == 0:
-##                         self.beli.grid(row=x, column=y)
-##       ta del še ne dela                  
+##          self.beli = Button(master, text="beli") 
+##          for x in range(125/2,1000, 125):
+##              for y in range(125/2,250 + 125/2, 125):
+##                  if (x+y) % 2 == 0:
+##                    self.beli.grid(row=x, column=y)
+##             
+##          self.pozdrav = StringVar(master, value="DAMA")     
+##          Label(master, textvariable=self.pozdrav).grid(row=900, column=50)
                     
-                    
+##          na koncu inita: izbira igralcev (veže na funkcijo izbira_igralca,
+##          ta dela naprej)
+        
+        def premakni_figuro(self,i,j):
+            pass
 
-        #naredi šahovsko plošèo
-        def premakni(self):
-           pass
+        def izbira_igralca(self):
+            pass
+
+        def zacni_igro(self):
+            pass
+
+        def koncaj_igro(self,zmagovalec):
+            pass
             
             
 
@@ -45,6 +54,6 @@ class Plosca():
  #GLAVNI PROGRAM
 root = Tk()
 root.title("Dama")
-aplikacija = Plosca()
+aplikacija = Gui()
 root.mainloop()
 # more bit Gui(root), sam piÅ¡e da nima argumentov
