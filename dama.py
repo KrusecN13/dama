20358c37fbe19829284dfa8ecf9c238d5ba52843
unknown <MarkunJ13@3Nad-01.std.fmf.uni-lj.si>
2016-03-07 18:40:11 +0100

    definiraj premakni in pojej

--- dama.py
+++ dama.py
@@ -41,19 +41,30 @@ class Igra():
                       [B, False, B, False, B, False, B, False],
                       [False, B, False, B, False, B, False, B]]
         
+        
         self.na_potezi = igralec_C
         self.zgodovina = [(self.deska, igralec_C)]
         
     def shrani_potezo(self):
 # seznamu zgodovina pripne par (trenutna pozicija na deski, igralec na potezi)
-        self.zgodovina.append((self.deska, self.na_potezi))
+        a = [self.deska[j][:] for j in range(8)]
+        self.zgodovina.append((a, self.na_potezi))
 
     
     def razveljavi(self):
 # ce igra clovek proti cloveku se razveljavi ena poteza, sicer pa dve(SE DODATI!!)
         (self.deska, self.na_potezi) = self.zgodovina.pop()
+##??? ali ta funkcija ze avtomatsko vzame zadnji element 'nove' zgodovine?
+##?? ali celoten seznam zgodovine
+
 
 # pop odstrani zadnji element seznama
+
+    def kopija(self):
+        copy = Igra()
+        copy.deska = [self.deska[j][:] for j in range(8)]
+        copy.na_potezi = self.na_potezi
+        return copy
 
     def je_veljavna(self, i, j):
         for i in range(8):
@@ -66,17 +77,94 @@ class Igra():
     def zmagovalec(self):
         pass
     
-    def pojej(self, i, j):
-        pass
-    
+    def pojej(self, druga):
+        iks = self.x
+        ipsilon = self.y
+        zeta = druga.x
+        teta = druga.y
+##        if self.dama:
+##            
+##            if abs(self.x - i)==1 and abs(self.y - j) == 1 and 0<=i<=7 and 0<=j<=7
+##            and :
+##                self.x = i
+##                self.y = j
+##                zbrisi(iks, ipsilon)
+        if self.barva == crna:
+            if druga.barva == bela:
+                if self.x - druga.x == 1 and (self.y + 1) == druga.y and self.deska[self.x -2 ][self.y +2] == None:
+                    self.x = iks - 2
+                    self.y = ipsilon + 2
+                    
+                    zbrisi(iks, ipsilon)
+                    zbrisi(zeta, teta)
+                if self.x - druga.x == -1 and (self.y + 1) == druga.y and self.deska[self.x + 2][self.y +2] == None:
+                    self.x = iks + 2
+                    self.y = ipsilon + 2
+                    
+                    zbrisi(iks, ipsilon)
+                    zbrisi(zeta, teta)
+        if self.barva == bela:
+            if druga.barva == crna:
+                if self.x - druga.x == 1 and (self.y - 1) == druga.y and self.deska[self.x -2 ][self.y -2] == None:
+                    self.x = iks - 2
+                    self.y = ipsilon - 2
+                    
+                    zbrisi(iks, ipsilon)
+                    zbrisi(zeta, teta)
+                if self.x - druga.x == -1 and (self.y - 1) == druga.y and self.deska[self.x + 2][self.y -2] == None:
+                    self.x = iks + 2
+                    self.y = ipsilon - 2
+                    
+                    zbrisi(iks, ipsilon)
+                    zbrisi(zeta, teta)
+                
+            
+
+            
+    def zbrisi(self,i,j):
+        self.deska[i][j] = None
+        
     def premakni(self, i, j):
-        pass
+        iks = self.x
+        ipsilon = self.y
+        if not je_veljavna(self, i, j):
+            return None
+        
+        if self.dama:
+            if abs(self.x - i)==1 and abs(self.y - j) == 1 and 0<=i<=7 and 0<=j<=7:
+                self.x = i
+                self.y = j
+                zbrisi(iks, ipsilon)    
+                
+                
+        elif self.barva == crna:
+            if abs(self.x - i)==1 and (self.y + 1) == j and 0<=i<=7 and 0<=j<=7:
+                self.x = i
+                self.y = j
+                zbrisi(iks, ipsilon)
+
+        elif self.barva == bela:
+            if abs(self.x - i)==1 and (self.y - 1) == j and 0<=i<=7 and 0<=j<=7:
+                self.x = i
+                self.y = j
+                zbrisi(iks, ipsilon)            
+                
+
+                   
     
     def naredi_potezo(self, i, j):
     # če je poteza neveljavna ne naredi ničesar
         if not je_veljavna(self, i, j):
             return None
+        
     # če je poteza veljavna jo izvede
+
+
+
+
+
+
+
         
 ##########################            
 
