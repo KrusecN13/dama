98f2b9ab37b010077b7869a281b29230e17f45bd
KrusecN13 <nika.krusec@gmail.com>
2016-03-23 13:21:26 +0100

    zacni, prekini, funkcija postavi figure

--- gui.py
+++ gui.py
@@ -3,12 +3,19 @@
 #Celotni razred skupaj z metodami bo vsebovan v (glavni) datoteki dama.py.
 
 
-from Tkinter import *
+from tkinter import *
 from dama import *
 
 #Definiramo razred, ki predstavlja našo aplikacijo
 class Gui():
+
+        TAG_FIGURA = 'figura'
+        
         def __init__(self, master=None):
+                self.igra = None
+                
+                self.igrc = None
+                self.igrb = None
             #init = stvari ki se zgodijo enkrat, vse kar se more zgodit, da
             #zaženemo grafični vmesnik
                 master.configure(background = 'orange')
@@ -34,10 +41,10 @@ class Gui():
                 nova_igra_menu.add_command(label = "Racunalnik - Racunalnik",
                                            command = lambda: self.zacni_igro(Racunalnik(self, Minimax(globina)),
                                                                              Racunalnik(self, Minimax(globina))))
-                zapri_menu.add_command(label = "Izhod", command = master.destroy)
+                zapri_menu.add_command(label = "Izhod", command = lambda: self.prekini_vse())
 
-                self.canvas = Canvas(master, width=800, height=800)
-                self.canvas.grid()
+                self.kanvas = Canvas(master, width=800, height=800)
+                self.kanvas.grid()
 
                 self.napis = StringVar(master, value = "Dama")
                 Label(master, textvariable = self.napis, background = 'orange').grid(row=1, column=0)
@@ -46,49 +53,85 @@ class Gui():
 
                 self.ime_igralcaC = StringVar(master, value = 'Crni igralec')
                 self.ime_igralcaB = StringVar(master, value = 'Beli igralec')
-                canvas_ime_igralcaC = Entry(master, width = 10, textvariable = self.ime_igralcaC, background = 'orange')
-                canvas_ime_igralcaB = Entry(master, width = 10, textvariable = self.ime_igralcaB, background = 'orange')
-                canvas_ime_igralcaC.grid()
-                canvas_ime_igralcaB.grid()
+                kanvas_ime_igralcaC = Entry(master, width = 10, textvariable = self.ime_igralcaC, background = 'orange')
+                kanvas_ime_igralcaB = Entry(master, width = 10, textvariable = self.ime_igralcaB, background = 'orange')
+                kanvas_ime_igralcaC.grid()
+                kanvas_ime_igralcaB.grid()
 
+                
+                self.postavi_figure()
                 slovar_figur = {}
-                for x in range(0,800,100):
-                        for y in range(0,800,100):
-                                if ((x+y)//100)%2 == 0:
-                                        self.canvas.create_rectangle(x+(100),y+(100),
-                                                    x,y,
-                                                    outline="#000000", fill="#000000")
-
-                                else:
-                                       self.canvas.create_rectangle(x+(100),y+(100),
-                                                    x,y,
-                                                    outline="#ffffff", fill="#CD8527")
                 for i in range(8):
                         for j in range(3):
                                 if (i+j)%2 == 0:
-                                        a = self.canvas.create_oval(i*100 + 15, j*100 + 15, i*100 + 85, j*100 + 85, fill='#7D26D9', outline='#000000') 
+                                        a = self.kanvas.create_oval(i*100 + 15, j*100 + 15, i*100 + 85, j*100 + 85, fill='#7D26D9', outline='#000000') 
                                         slovar_figur[Figura('igrC')]=a
                 for i in range(8):
                         for j in range(5,8):
                                 if (i+j)%2 == 0:
-                                        a = self.canvas.create_oval(i*100 + 15, j*100 + 15, i*100 + 85, j*100 + 85, fill='#9EB5BA', outline='#000000') 
+                                        a = self.kanvas.create_oval(i*100 + 15, j*100 + 15, i*100 + 85, j*100 + 85, fill='#9EB5BA', outline='#000000') 
                                         slovar_figur[Figura('igrB')]=a
                                         
-                print(slovar_figur)
+                #print(slovar_figur)
 ##          na koncu inita: izbira igralcev (veže na funkcijo izbira_igralca,
 ##          ta dela naprej)
+#coords
+
+        def postavi_figure(self):
+                for x in range(0,800,100):
+                        for y in range(0,800,100):
+                                if ((x+y)//100)%2 == 0:
+                                        self.kanvas.create_rectangle(x+(100),y+(100),
+                                                    x,y,
+                                                    outline="#000000", fill="#000000")
+
+                                else:
+                                       self.kanvas.create_rectangle(x+(100),y+(100),
+                                                    x,y,
+                                                    outline="#ffffff", fill="#CD8527")
+
+
+                
+        def zacni_igro(self, igrc, igrb):
+        # najprej ustavimo vsa vlakna, ki še razmišljajo ter pobrišemo polje
+                self.prekini_igralca()
+                self.kanvas.delete(Gui.TAG_FIGURA)
+                self.postavi_figure()
+                
+
+                self.igra = Igra()
+                
+                self.igrc = igrc
+                self.igrb = igrb
+
+                self.napis.set("Na potezi je crni")
+                self.igrc.igraj()
+                
+                
+            
         
-        def premakni_figuro(self,i,j):
+        def naredi_potezo(self,a,p):
+        # a so stare koordinate, ki jih dobimo s klikom, p pa nove 
             pass
 
         def izbira_igralca(self):
             pass
 
-        def zacni_igro(self):
-            pass
-
         def koncaj_igro(self,zmagovalec):
             pass
+
+        def prekini_vse(self):
+# zapreti okno, prekiniti igralce, 
+                logging.debug("prekinjam igralce")
+                self.igra.na_potezi.prekini()
+                master.destroy()
+                
+        def prekini_igralca(self):
+                pass
+
+        
+
+        
             
             
 
